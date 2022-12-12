9ac7e7d59bcc35294bd52eabcbc120af8e5df48a
TimelessJiuYue <746493196@qq.com>
2022-12-12 17:04:53 +0800

    .

--- Daliy.c/Daliy.c.cpp
+++ Daliy.c/Daliy.c.cpp
@@ -1,360 +1,141 @@
-﻿//#include <stdio.h>
-//int main()
-//{
-//	int day = 0;
-//	scanf_s("%d", &day);
-//	switch (day)
-//	{
-//	case 1:
-//	case 2:
-//	case 3:
-//	case 4:
-//	case 5:
-//		printf("工作日\n");
-//		break;
-//	case 6:
-//	case 7:
-//		printf("周末\n");
-//	default:
-//		break;
-//	}
-//	
-//}
-//#include <stdio.h>
-//
-//int main()
-//{
-//	int x, z;
-//	int y = 10;
-//	scanf_s("%d", &x);
-//	z = x / y;
-//	printf("%d\n", z);
-//
-//}
-//{
-//	scanf_s("%d", &z);
-//	int z;
-//	if (z > 10 && z < 0);
-//	printf("无效输入\n");
-//	else if (z >= 0 && z <= 10);
-//	{
-//		switch (z)
-//		{
-//		case 10:A
-//		case 9:B
-//		case 8:C
-//		case 7:D
-//		case 6:E
-//			break
-//		case 5:不及格
-//
-//
-//
-//		default:
-//			break;
-//		}
-//	}
-//	return 0;
-//}
-//#include <stdio.h>
-//int main()
-//{
-//	int line = 0;
-//	printf("加入比特\n");
-//	while (line < 2000)
-//	{
-//		printf("敲一行代码: %d\n", line);
-//		line++;
-//	}
-//	if (line >= 2000)
-//	printf("好offer\n");
-//}
-#include <stdio.h>
-//int main()
-//{
-//	int C;
-//	scanf_s("%d", &C);
-//	if (C > 100 && C < 0)
-//	{
-//		printf("无效输入\n");
-//	}
-//	else
-//	{
-//		C = C / 10;
-//	}
-//	switch (C)
-//	{
-//	case 10:
-//	case 9:
-//	case 8:
-//	case 7:
-//	case 6:
-//		printf("还行\n");
-//		break;
-//	case 5:
-//		printf("不行\n");
-//
-//
-//	default:
-//		break;
-//	}
-//}
-//int main()
-//{
-//	char 字符;
-//	字符 = getchar();
-//	if (字符>='a' && 字符<='z')
-//	{
-//		字符 -= 32;
-//		printf("%c\n", 字符);
-//			return 0;
-//	}
-//
-//}
-/*int main()
+﻿#include <stdio.h>
+void menu()
 {
-	int line = 0;
-	for ( line = 0; line < 1000; line++)
-	{
-		printf("好offer\n");
-	}
-}*/
-//int main()
-//{
-//	int i = 0;
-//	while (i<=100)
-//	{
-//		i = i + 1;
-//		printf("%d", i);
-//	}
-//#include <math.h>
+	printf("********************************************\n");
+	printf("****          请输入选项编号(0-5)       ****\n");
+	printf("********************************************\n");
+	printf("****        1——创建商品档案           ****\n");
+	printf("****        2——编辑商品信息           ****\n");
+	printf("****        3——统计销售情况           ****\n");
+	printf("****        4——查询商品信息           ****\n");
+	printf("****        5——显示商品信息           ****\n");
+	printf("****        0——退出系统               ****\n");
+	printf("********************************************\n");
+}
+void 创建商品档案()
+{
 
-//int main()
+}
+void 编辑商品信息()
+{
+	printf("********************************************\n");
+	printf("****         请输入选项编号(0-4)        ****\n");
+	printf("********************************************\n");
+	printf("****         1——添加新商品            ****\n");
+	printf("****         2——删除原有商品          ****\n");
+	printf("****         3——修改原有商品信息      ****\n");
+	printf("****         4——返回                 ****\n");
+	printf("****         0——退出                 ****\n");
+	printf("********************************************\n");
 
-/* {
-	int i, sum=0;
-	for ( i = 0; i <= 100; i++)
-	{
-		sum += i;
-	}
-	printf("%d", sum);
-	return 0;
+}
+void 添加新商品()
+{
 
-}*/
-//{
-	//double d = 300000, p = 6000, r = 0.01, m;
-	//m = log10(p / (p - d * r)) / log10(1 + r);
-	//printf("m=%5.2\n",m);
-	//
-//int main()
-//{
-	//char c1, c2;
-	//c1 = 197;
-	//c2 = 198;
-	//printf("c1=%c,c2=%c\n", c1, c2);
-	//printf("c1=%d,c2=%d\n", c1, c2);
-	//return 0;
-//int main()
-//{
-//	int c1,c2;
-//	printf("请输入两个字符c1,c2;\n");
-//	c1 = getchar();
-//	c2 = getchar();
-//	printf("%d\n",c2);
-//	putchar(%c1);
-//	return 0;
-//}
-//int main()
-//{
-//	int n;
-//	int count = 0;
-//	scanf_s("%d", &n);
-//	while (n != 0)
-//	{
-//		
-//		n = n / 10;
-//		count++;
-//		printf("%d", n);
-//	}
-//	
+}
+void 统计销售情况()
+{
 
-
-//int main()
-//{
-//	int n;
-//	int count = 0;
-//	scanf_s("%d", &n);
-//	for (count =0; count=(count/10)!=0; count++)
-//	{
-//		
-//	}
-//	printf("%d", count);
-//}
-//
-//int main()
-//{
-//	int n;
-//	scanf_s("%d", &n);
-//	if (n=0)
-//	{
-//		printf("%d", n);
-//	}
-//	else
-//	{
-//		n = n % 10;
-//		printf("%d", n);
-//
-//	}
-//}
-//int main()
-//{
-//	int n;
-//	int sum = 0;
-//	scanf_s("%d", &n);
-//	while (n<=10)
-//	{
-//		if (n!=0)
-//		{
-//			n = n * (n - 1);
-//			n--;
-//			sum == n;
-//		}
-//		printf("%d", sum);
-//		return 0;
-//	}
-//}
-//int main()
-//{
-//	int n = 0;
-//	int x = 0, y = 0, z = 0;
-//	for ( n = 0; n < 1000; n++)
-//	{
-//		x = n % 10;
-//		y = (n / 10) % 10;
-//		z = n / 100;
-//		if ((x * x * x + y * y * y + z * z * z) == n);
-//		{
-//			printf("%d\n", n);
-//		}
-//		
-//	}
-//	return 0;
-//	
-//}
-//int main()
-//{
-//	int i, j, k, n, sumi, sumk, sumj;
-//	printf("\n100-1000 包含的水仙花数分别有:  \n\n");
-//	for ( n = 100; n < 1000; n++)
-//	{
-//		i = n / 100;
-//		j = n / 10 % 10;
-//		k = n % 10;
-//		sumi = i * i * i;
-//		sumj = j * j * j;
-//		sumk = k * k * k;
-//		if (n == sumi + sumk + sumj);
-//		{
-//			printf("%-5d", n);
-//		}
-//
-//	}
-//	printf("\n");
-//	return 0;
-//}
-//
-//main()
-//{
-//	int i;
-//	double r;
-//	r = 5.0;
-//	printf("\n");
-//	for ( i = 0; i <=9; i++)
-//	{
-//		printf("%10.71f\n", rndl(&r));
-//	}
-//	printf("\n");
-//}
-//#include<stdlib.h>
-//int main()
-//{
-//    int n = 0;
-//    int i, j, k;
-//    printf("你想输出菱形的行数是奇数几：");
-//    scanf_s("%d", &n);
-//    printf("你想要的菱形如下：\n");
-//    for (i = 1; i <= (n / 2 + 1); i++) //上三角
-//    {
-//        for (j = 1; j <= (n / 2 + 1 - i); j++)
-//            printf(" ");       //输出*前面的空格
-//        for (k = 1; k <= (2 * i - 1); k++)
-//            printf("*");		   //输出*
-//        printf("\n");
-//    }
-//    for (i = 1; i <= n / 2; i++)    //下三角
-//    {
-//        for (j = 1; j <= i; j++)
-//            printf(" ");      //输出*前面的空格
-//        for (k = 1; k <= (n - 2 * i); k++)
-//            printf("*");       //输出*
-//        printf("\n");
-//    }
-//    system("pause");
-//    return 0;
-//}
-//int main()
-//{
-//	char a = 'A',t;
-//	int len = 8;
-//	printf("%c\n", a);
-//	for (int i = 0; i < sizeof(a)*8; i++)
-//	{
-//		t = a;
-//		t >>= len - i - 1;
-//		t = t & 1;
-//		printf("%d", t > 0 ? 1 : 0);
-//
-//	}
-//	printf("\n");
-//	return 0;
-//}
-//void add(int a,int b)
-//{
-//	int c = 0;
-//	scanf_s("%d", &a);
-//	scanf_s("%d", &b);
-//	c = a + b;
-//	printf("%d", c);
-//}
-//int main()
-//{
-//	int a = 0;
-//	int b = 0;
-//	int c = 0;
-//	
-//	add(a, b);
-//	
-//	
-//}
-
+}
+void 查询商品信息()
+{
+	printf("********************************************\n");
+	printf("****         请输入选项编号(0-3)         ****\n");
+	printf("********************************************\n");
+	printf("****         1——按品名查询             ****\n");
+	printf("****         2——按剩余数查询           ****\n");
+	printf("****         3——返回                   ****\n");
+	printf("****         0——退出                   ****\n");
+	printf("********************************************\n");
+}
 int main()
 {
-	int i, j, k;
-	for (i = 0; i <= 3; i++)
+	int input = 0;
+	do
 	{
-		for(j=0;j<=2-i;j++)
-		printf(" ");
-		for (k = 0; k <= 2 * i; k++)
-			printf("*");
-		printf("\n");
-	}
-	for (i = 0; i <= 2; i++);
-	{
-		for (j = 0; j <= i; j++);
-			printf(" ");
-		for (k = 0; k <= 4 - 2 * i; k++)
-			printf("*");
-		printf("\n");
-	}
-	return 0;
+		menu();
+		printf("请输入选项编号(0-5):");
+		scanf_s("%d", &input);
+		switch (input)
+		{
+		case 0:
+			printf("退出程序");
+			return 0;
+			break;
+		case 1:创建商品档案();
+				break;
+		case 2:编辑商品信息();
+			printf("是否继续进行当前进程[1是/2否]\n");
+			scanf_s("%d", &input);
+			switch (input)
+			{
+			case 1://进入编辑商品信息
+				printf("继续进行\n");
+				编辑商品信息();
+				scanf_s("%d", &input);
+				switch (input)//开始选择
+				{
+				case 4:
+					menu();
+					break;
+				case 0:
+					printf("退出程序\n");
+					return 0;
+				//case 1://添加新商品
+				//	添加新商品();
+				//	printf("是否继续进行当前进程[1是/2否]");
+				//	scanf_s("%d", &input);
+				//	switch (input)
+				//	{
+				//	case 1:
+				//		printf("继续进行\n");
+				//		添加新商品();
+				//		break;
+				//	case 2:
+				//		printf("取消进行\n");
+				//		break;
+				//	default:
+				//		printf("输入错误\n");
+				//		break;
+				//	break;
+				//	}
+
+				default:
+					printf("输入错误\n");
+					break;
+				}
+				break;
+			case 2:
+				printf("取消进行\n");
+				
+				break;
+			default:
+				printf("输入错误\n");
+				break;
+			}
+			break;
+		case 3:统计销售情况();
+				break;
+		case 4:查询商品信息();
+			printf("是否继续进行当前进程[1是/2否]");
+			scanf_s("%d", &input);
+			switch (input)
+			{
+			case 1:
+				printf("继续进行\n");
+				查询商品信息();
+			default:
+				break;
+			}
+				break;
+		case 5:
+				break;
+		default:
+			printf("输入错误");
+			break;
+		}
+		
+	} 
+	while (input);
+	
 }
